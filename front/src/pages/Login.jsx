f93c50dffb5f52b9a138032a5c50a7f5f6da67fd
shahnawaz patel <shahnawazpatel23@gmail.com>
2025-01-10 03:30:49 +0530

    front url

--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://task-nodejs-server.vercel.app/api';
 
 const Login = () => {
   const [username, setUsername] = useState('');
--- front/src/pages/ResetPassword.jsx
+++ front/src/pages/ResetPassword.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://task-nodejs-server.vercel.app/api';
 
 const ResetPassword = () => {
   const [formData, setFormData] = useState({
